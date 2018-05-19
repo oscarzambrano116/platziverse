5c24d62326217aa6576e1de20b1350d79e27930b
Oscar Zambrano <oscarz.developer@gmail.com>
2018-05-19 18:57:38 -0400

    Proxy api routes

---
+++ platziverse-web/config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
+  apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNTI2NzcwMzE4fQ.z25YRqaA72KIKjsQqRCwt-7myR7vx-Sj_IrRI7vtlQU'
+}
