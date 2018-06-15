41200055d8bf521a488deab72a2b61cded49d7cb
Oscar Zambrano <oscarz.developer@gmail.com>
2018-06-14 21:02:37 -0500

    platziverse-mqtt as a role in platziverse-deploy

---
+++ platziverse-deploy/roles/platziverse-db/files/platziverse-db/setup.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const minimist = require('minimist')
+const db = require('./')
+
+const args = minimist(process.argv)
+const prompt = inquirer.createPromptModule()
+
+async function setup () {
+  if (!args.yes) {
+    const answer = await prompt([
+      {
+        type: 'confirm',
+        name: 'setup',
+        message: 'This will destroy your database, are you sure?'
+      }
+    ])
+
+    if (!answer.setup) {
+      return console.log('Nothing happened :)')
+    }
+  }
+
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASS || 'admin',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: msg => debug(msg),
+    setup: true
+  }
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+  process.exit(0)
+}
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[Faltal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+setup()
